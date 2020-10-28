90ff2b1802ddebc3bf699332b8e24866e84591f8
Andrew121410 <andrewcherring@gmail.com>
2020-10-28 14:48:26 -0400

    1.16.3! + Drop support for 1.16.1.

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
@@ -3,19 +3,15 @@ package com.andrew121410.mc.world16utils.utils;
 import com.andrew121410.mc.world16utils.World16Utils;
 import com.andrew121410.mc.world16utils.blocks.BlockUtils;
 import com.andrew121410.mc.world16utils.blocks.BlockUtils_V1_12_R1;
-import com.andrew121410.mc.world16utils.blocks.BlockUtils_V1_16_R1;
 import com.andrew121410.mc.world16utils.blocks.BlockUtils_V1_16_R2;
 import com.andrew121410.mc.world16utils.blocks.sign.SignUtils;
 import com.andrew121410.mc.world16utils.blocks.sign.SignUtils_V1_12_R1;
-import com.andrew121410.mc.world16utils.blocks.sign.SignUtils_V1_16_R1;
 import com.andrew121410.mc.world16utils.blocks.sign.SignUtils_V1_16_R2;
 import com.andrew121410.mc.world16utils.enchantment.EnchantmentUtils;
 import com.andrew121410.mc.world16utils.enchantment.EnchantmentUtils_V1_12_R1;
-import com.andrew121410.mc.world16utils.enchantment.EnchantmentUtils_V1_16_R1;
 import com.andrew121410.mc.world16utils.enchantment.EnchantmentUtils_V1_16_R2;
 import com.andrew121410.mc.world16utils.player.SmoothTeleport;
 import com.andrew121410.mc.world16utils.player.SmoothTeleport_V1_12_R1;
-import com.andrew121410.mc.world16utils.player.SmoothTeleport_V1_16_R1;
 import com.andrew121410.mc.world16utils.player.SmoothTeleport_V1_16_R2;
 import org.bukkit.Bukkit;
 
@@ -34,12 +30,6 @@ public class ClassWrappers {
                 this.signUtils = new SignUtils_V1_16_R2(plugin);
                 this.smoothTeleport = new SmoothTeleport_V1_16_R2();
                 this.enchantmentUtils = new EnchantmentUtils_V1_16_R2();
-                break;
-            case "v1_16_R1":
-                this.blockUtils = new BlockUtils_V1_16_R1();
-                this.signUtils = new SignUtils_V1_16_R1(plugin);
-                this.smoothTeleport = new SmoothTeleport_V1_16_R1();
-                this.enchantmentUtils = new EnchantmentUtils_V1_16_R1();
                 break;
             case "v1_12_R1":
                 this.blockUtils = new BlockUtils_V1_12_R1();
