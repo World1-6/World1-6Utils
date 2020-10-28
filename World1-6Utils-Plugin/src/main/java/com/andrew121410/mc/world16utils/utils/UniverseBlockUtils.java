8bf7c634afd4170effe0742bacf61d49c6c221e1
Andrew121410 <andrewcherring@gmail.com>
2020-10-28 16:32:06 -0400

    Tiny fix.

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/UniverseBlockUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/UniverseBlockUtils.java
@@ -1,11 +1,10 @@
 package com.andrew121410.mc.world16utils.utils;
 
 import com.cryptomorin.xseries.XMaterial;
-import org.bukkit.Material;
 
 public class UniverseBlockUtils {
 
     public static boolean isFarmLand(XMaterial xMaterial) {
-        return xMaterial.parseMaterial() == Material.FARMLAND;
+        return xMaterial.parseMaterial() == XMaterial.FARMLAND.parseMaterial();
     }
 }
