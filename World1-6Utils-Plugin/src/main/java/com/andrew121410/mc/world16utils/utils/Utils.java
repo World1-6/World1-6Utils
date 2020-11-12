af16c46dc4cfa7ddd6f36286622c0b2b577fee1d
Andrew121410 <andrewcherring@gmail.com>
2020-11-12 17:26:45 -0500

    Better Utils.java

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/Utils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/Utils.java
@@ -1,6 +1,6 @@
 package com.andrew121410.mc.world16utils.utils;
 
-import org.bukkit.Bukkit;
+import java.util.List;
 
 public class Utils {
 
@@ -10,77 +10,137 @@ public class Utils {
     public static final String DEBUG_TAG = PREFIX + "->[&eDEBUG&r]";
     public static final String EMERGENCY_TAG = PREFIX + "->&c[EMERGENCY]&r";
 
+    public static boolean isByte(String input) {
+        try {
+            Byte.parseByte(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Byte asByteOrElse(String input, Byte fallback) {
+        try {
+            return Byte.parseByte(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isShort(String input) {
+        try {
+            Short.parseShort(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Short asShortOrElse(String input, Short fallback) {
+        try {
+            return Short.parseShort(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Integer asIntegerOrElse(String input, Integer fallback) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isLong(String input) {
+        try {
+            Long.parseLong(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Long asLongOrElse(String input, Long fallback) {
+        try {
+            return Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isFloat(String input) {
+        try {
+            Float.parseFloat(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Float asFloatOrElse(String input, Float fallback) {
+        try {
+            return Float.parseFloat(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isDouble(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Double asDoubleOrElse(String input, Double fallback) {
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static boolean isBoolean(String input) {
+        try {
+            Boolean.parseBoolean(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static Boolean asBooleanOrElse(String input, Boolean fallback) {
+        try {
+            return Boolean.parseBoolean(input);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
+    }
+
+    public static String getIndexFromStringList(List<String> stringList, int index) {
+        if (index >= 0 && index < stringList.size()) {
+            return stringList.get(index);
+        }
+        return null;
+    }
+
     public String getLastStrings(String myString, int index) {
         if (myString.length() > index)
             return myString.substring(myString.length() - index);
         else
             return myString;
-    }
-
-    public boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean isLong(String input) {
-        try {
-            Long.parseLong(input);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean isBoolean(String boolean1) {
-        return boolean1.equalsIgnoreCase("true") || boolean1.equalsIgnoreCase("false");
-    }
-
-    public Integer asIntOrDefault(String input, int default1) {
-        try {
-            Integer.parseInt(input);
-            return Integer.valueOf(input);
-        } catch (Exception e) {
-            return default1;
-        }
-    }
-
-    public Long asLongOrDefault(String input, long default1) {
-        try {
-            Long.parseLong(input);
-            return Long.valueOf(input);
-        } catch (Exception e) {
-            return default1;
-        }
-    }
-
-    public Double asDoubleOrDefault(String input, double default1) {
-        try {
-            Double.parseDouble(input);
-            return Double.valueOf(input);
-        } catch (Exception e) {
-            return default1;
-        }
-    }
-
-    public float asFloatOrDefault(String input, float default1) {
-        try {
-            Float.parseFloat(input);
-            return Float.parseFloat(input);
-        } catch (Exception e) {
-            return default1;
-        }
-    }
-
-    public boolean asBooleanOrDefault(String boolean1, boolean default1) {
-        try {
-            return Boolean.parseBoolean(boolean1);
-        } catch (Exception e) {
-            return default1;
-        }
     }
 
     public boolean isClass(String className) {
@@ -89,16 +149,6 @@ public class Utils {
             return true;
         } catch (ClassNotFoundException e) {
             return false;
-        }
-    }
-
-    public Class<?> getNMSClass(String name) {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-        try {
-            return Class.forName("net.minecraft.server." + version + "." + name);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
         }
     }
 }
