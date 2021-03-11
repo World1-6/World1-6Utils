89b38eb61e4226513f58444912b4ff0c6d591920
Andrew121410 <andrewcherring@gmail.com>
2021-03-11 11:44:09 -0500

    More tiny changes.

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/gui/advance/AdvanceGUIWindow.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/gui/advance/AdvanceGUIWindow.java
@@ -37,10 +37,10 @@ public abstract class AdvanceGUIWindow extends GUIWindow {
     public boolean onSlotClicked(InventoryClickEvent event) {
         if (event == null) return false;
         if (event.getCurrentItem() == null) return false;
-        GUIItem simpleGUIItem = this.guiItemMap.get(event.getSlot());
-        if (simpleGUIItem != null) {
-            if (simpleGUIItem.getConsumer() != null) {
-                simpleGUIItem.getConsumer().accept(event);
+        GUIItem guiItem = this.guiItemMap.get(event.getSlot());
+        if (guiItem != null) {
+            if (guiItem.getConsumer() != null) {
+                guiItem.getConsumer().accept(event);
                 return true;
             }
         }
@@ -48,35 +48,41 @@ public abstract class AdvanceGUIWindow extends GUIWindow {
     }
 
     public void update(Map<Integer, GUIItem> guiItemMap) {
-        this.guiItemMap = guiItemMap;
+        update(guiItemMap, null, null);
+    }
+
+    public void update(Map<Integer, GUIItem> guiItemMap, String name) {
+        update(guiItemMap, name, null);
     }
 
     public void update(Map<Integer, GUIItem> guiItemMap, int slots) {
-        this.guiItemMap = guiItemMap;
-        this.slots = slots;
+        update(guiItemMap, null, slots);
     }
 
-    public void update(Map<Integer, GUIItem> guiItemMap, String name, int slots) {
-        this.guiItemMap = guiItemMap;
-        this.name = name;
-        this.slots = slots;
+    public void update(Map<Integer, GUIItem> guiItemMap, String name, Integer slots) {
+        if (guiItemMap != null) this.guiItemMap = guiItemMap;
+        else this.guiItemMap = new HashMap<>();
+        if (name != null) this.name = name;
+        if (slots != null) this.slots = slots;
     }
 
     public void update(List<GUIItem> list) {
-        this.guiItemMap = new HashMap<>();
-        for (GUIItem simpleGUIItem : list) this.guiItemMap.putIfAbsent(simpleGUIItem.getSlot(), simpleGUIItem);
+        update(list, null, null);
+    }
+
+    public void update(List<GUIItem> list, String name) {
+        update(list, name, null);
     }
 
     public void update(List<GUIItem> list, int slots) {
-        this.guiItemMap = new HashMap<>();
-        for (GUIItem simpleGUIItem : list) this.guiItemMap.putIfAbsent(simpleGUIItem.getSlot(), simpleGUIItem);
-        this.slots = slots;
+        update(list, null, slots);
     }
 
-    public void update(List<GUIItem> list, String name, int slots) {
+    public void update(List<GUIItem> list, String name, Integer slots) {
         this.guiItemMap = new HashMap<>();
-        for (GUIItem simpleGUIItem : list) this.guiItemMap.putIfAbsent(simpleGUIItem.getSlot(), simpleGUIItem);
-        this.name = name;
-        this.slots = slots;
+        if (list != null)
+            for (GUIItem simpleGUIItem : list) this.guiItemMap.putIfAbsent(simpleGUIItem.getSlot(), simpleGUIItem);
+        if (name != null) this.name = name;
+        if (slots != null) this.slots = slots;
     }
 }
