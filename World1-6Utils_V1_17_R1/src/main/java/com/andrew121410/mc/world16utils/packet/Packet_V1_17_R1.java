e59c32d910a3cf2bb2f02ddd6543630d9b6c8415
Andrew121410 <andrewcherring@gmail.com>
2021-06-19 00:28:24 -0400

    Add marker support2

---
+++ World1-6Utils_V1_17_R1/src/main/java/com/andrew121410/mc/world16utils/packet/Packet_V1_17_R1.java
@@ -0,0 +1,54 @@
+package com.andrew121410.mc.world16utils.packet;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import io.netty.buffer.Unpooled;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Packet_V1_17_R1 implements IPackets {
+
+    private ProtocolManager protocolManager;
+
+    public Packet_V1_17_R1() {
+        this.protocolManager = ProtocolLibrary.getProtocolManager();
+    }
+
+    @Override
+    public void sendDebugCreateMarkerPacket(Player player, Location blockPosition, int color, String message, int duration) {
+        FriendlyByteBuf friendlyByteBuf = new FriendlyByteBuf(Unpooled.buffer());
+
+        friendlyByteBuf.writeBlockPos(new BlockPos(blockPosition.getBlockX(), blockPosition.getBlockY(), blockPosition.getBlockZ()));
+        friendlyByteBuf.writeInt(color);
+        friendlyByteBuf.writeUtf(message);
+        friendlyByteBuf.writeInt(duration);
+
+        PacketContainer packetContainer = protocolManager.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+        packetContainer.getStrings().write(0, "minecraft:debug/game_test_add_marker");
+        packetContainer.getModifier().write(1, friendlyByteBuf);
+
+        try {
+            this.protocolManager.sendServerPacket(player, packetContainer);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendDebugGameTestClearPacket(Player player) {
+        PacketContainer packetContainer = protocolManager.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+        packetContainer.getStrings().write(0, "debug/game_test_clear");
+
+        try {
+            this.protocolManager.sendServerPacket(player, packetContainer);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
