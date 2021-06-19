afc34b26c29dbe69f2f3993b342d1f35abe25b1c
Andrew121410 <andrewcherring@gmail.com>
2021-06-19 00:27:56 -0400

    MarkerSupport1

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/blocks/UniversalBlockUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/blocks/UniversalBlockUtils.java
@@ -1,8 +1,8 @@
-package com.andrew121410.mc.world16utils.utils;
+package com.andrew121410.mc.world16utils.blocks;
 
 import com.cryptomorin.xseries.XMaterial;
 
-public class UniverseBlockUtils {
+public class UniversalBlockUtils {
 
     public static boolean isFarmLand(XMaterial xMaterial) {
         return xMaterial.parseMaterial() == XMaterial.FARMLAND.parseMaterial();
