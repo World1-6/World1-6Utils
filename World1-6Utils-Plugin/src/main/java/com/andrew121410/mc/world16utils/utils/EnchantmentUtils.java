903040d2e56751a10e1f890c75d57d7da3242a59
Andrew121410 <andrewcherring@gmail.com>
2021-12-16 12:48:11 -0500

    Make EnchantmentUtils.getByName static

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/EnchantmentUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/EnchantmentUtils.java
@@ -4,7 +4,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 
 public class EnchantmentUtils {
-    public Enchantment getByName(String name) {
+    public static Enchantment getByName(String name) {
         return Enchantment.getByKey(NamespacedKey.minecraft(name));
     }
 }
