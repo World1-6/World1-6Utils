5cbac3ae201402d6f6d1d2349646d359503bab31
Andrew121410 <andrewcherring@gmail.com>
2022-06-07 16:49:11 -0400

    Added 1.19 support

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
@@ -3,10 +3,13 @@ package com.andrew121410.mc.world16utils.utils;
 import com.andrew121410.mc.world16utils.World16Utils;
 import com.andrew121410.mc.world16utils.blocks.BlockUtils;
 import com.andrew121410.mc.world16utils.blocks.BlockUtils_V1_18_R2;
+import com.andrew121410.mc.world16utils.blocks.BlockUtils_V1_19_R1;
 import com.andrew121410.mc.world16utils.packet.IPackets;
 import com.andrew121410.mc.world16utils.packet.Packet_V1_18_R2;
+import com.andrew121410.mc.world16utils.packet.Packet_V1_19_R1;
 import com.andrew121410.mc.world16utils.player.SmoothTeleport;
 import com.andrew121410.mc.world16utils.player.SmoothTeleport_V1_18_R2;
+import com.andrew121410.mc.world16utils.player.SmoothTeleport_V1_19_R1;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit_723;
 import org.bukkit.Bukkit;
@@ -30,7 +33,15 @@ public class ClassWrappers {
                 //Extra
                 this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_723() : null;
             }
-            default -> throw new IllegalArgumentException("Unable to detect NMS version(" + version + ") for ClassWrappers");
+            case "v1_19_R1" -> {
+                this.blockUtils = new BlockUtils_V1_19_R1();
+                this.smoothTeleport = new SmoothTeleport_V1_19_R1();
+                this.packets = new Packet_V1_19_R1();
+                //Extra
+                this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_723() : null;
+            }
+            default ->
+                    throw new IllegalArgumentException("Unable to detect NMS version(" + version + ") for ClassWrappers");
         }
     }
 
