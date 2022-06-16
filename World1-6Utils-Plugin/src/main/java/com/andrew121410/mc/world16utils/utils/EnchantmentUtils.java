7fc01eb540aae970da8363940219adc352bdbcb7
Andrew121410 <andrewcherring@gmail.com>
2022-06-16 00:10:15 -0400

    Add EnchantmentUtils.getVanillaEnchantmentNames

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/EnchantmentUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/EnchantmentUtils.java
@@ -3,8 +3,19 @@ package com.andrew121410.mc.world16utils.utils;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnchantmentUtils {
     public static Enchantment getByName(String name) {
         return Enchantment.getByKey(NamespacedKey.minecraft(name));
+    }
+
+    public static List<String> getVanillaEnchantmentNames() {
+        List<String> names = new ArrayList<>();
+        for (Enchantment enchantment : Enchantment.values()) {
+            names.add(enchantment.getKey().getKey());
+        }
+        return names;
     }
 }
