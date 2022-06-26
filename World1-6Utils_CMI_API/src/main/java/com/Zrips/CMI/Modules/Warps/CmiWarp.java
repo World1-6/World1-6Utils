adfaec2d6a8f563f98d70fba430c3122dc42d5b8
Andrew121410 <andrewcherring@gmail.com>
2022-06-26 09:00:42 -0400

    Update CmiWarp

--- World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/Modules/Warps/CmiWarp.java
+++ World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/Modules/Warps/CmiWarp.java
@@ -2,10 +2,13 @@ package com.Zrips.CMI.Modules.Warps;
 
 import net.Zrips.CMILib.Container.CMILocation;
 
+import java.util.UUID;
+
 public class CmiWarp {
 
     private String name;
     private CMILocation loc;
+    private UUID creator;
 
     public CmiWarp(String name, CMILocation loc) {
         this.name = name;
@@ -20,4 +23,11 @@ public class CmiWarp {
         return name;
     }
 
+    public UUID getCreator() {
+        return creator;
+    }
+
+    public void setCreator(UUID creator) {
+        this.creator = creator;
+    }
 }
