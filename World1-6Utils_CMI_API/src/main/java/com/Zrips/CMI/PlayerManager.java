5eb26bf075b54308776c491d21f5f5021414442a
Andrew121410 <andrewcherring@gmail.com>
2022-06-26 10:07:10 -0400

    [CMI-API] Update PlayerManager

--- World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/PlayerManager.java
+++ World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/PlayerManager.java
@@ -1,12 +1,22 @@
 package com.Zrips.CMI;
 
 import com.Zrips.CMI.Containers.CMIUser;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class PlayerManager {
 
     public CMIUser getUser(UUID uuid) {
+        return null;
+    }
+
+    public CMIUser getUser(OfflinePlayer paramOfflinePlayer) {
+        return null;
+    }
+
+    public CMIUser getUser(Player paramPlayer) {
         return null;
     }
 }
