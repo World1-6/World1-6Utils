c19a094638a9299638764f1edf5d4e6d91e576f2
Andrew121410 <andrewcherring@gmail.com>
2022-07-26 22:14:01 -0400

    Laughing ok actually fix building

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
@@ -2,7 +2,6 @@ package com.andrew121410.mc.world16utils.utils;
 
 import com.andrew121410.mc.world16utils.World16Utils;
 import com.andrew121410.mc.world16utils.packet.IPackets;
-import com.andrew121410.mc.world16utils.packet.Packet_V1_18_R2;
 import com.andrew121410.mc.world16utils.packet.Packet_V1_19_R1;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit_7210;
@@ -18,11 +17,6 @@ public class ClassWrappers {
     public ClassWrappers(World16Utils plugin) {
         String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
         switch (version) {
-            case "v1_18_R2" -> {
-                this.packets = new Packet_V1_18_R2();
-                //Extra
-                this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_7210() : null;
-            }
             case "v1_19_R1" -> {
                 this.packets = new Packet_V1_19_R1();
                 //Extra
