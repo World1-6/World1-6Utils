45639dfe32ea237a7f08351c23188e3223af01c2
Andrew121410 <andrewcherring@gmail.com>
2022-09-25 16:58:13 -0400

    Oops

--- World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/Modules/SavedInv/CMIInventory.java
+++ World1-6Utils_CMI_API/src/main/java/com/Zrips/CMI/Modules/SavedInv/CMIInventory.java
@@ -3,10 +3,17 @@ package com.Zrips.CMI.Modules.SavedInv;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 public class CMIInventory {
 
+    public CMIInventory(String paramString, UUID paramUUID) {
+    }
+
     public HashMap<Integer, ItemStack> getItems() {
         return null;
+    }
+
+    public void setItems(HashMap<Integer, ItemStack> paramHashMap) {
     }
 }
