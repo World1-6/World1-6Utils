4270650852dbb67282bea3406d8615ae0efcdabd
Andrew121410 <andrewcherring@gmail.com>
2022-09-24 20:19:40 -0400

    Update aggainsdsd

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/BukkitSerialization.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/BukkitSerialization.java
@@ -1,9 +1,7 @@
 package com.andrew121410.mc.world16utils.utils;
 
-import org.bukkit.Bukkit;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
@@ -11,36 +9,11 @@ import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 // https://gist.github.com/graywolf336/8153678
 public class BukkitSerialization {
-
-    /**
-     * Converts the player inventory to a String array of Base64 strings. First string is the content and second string is the armor.
-     *
-     * @param playerInventory to turn into an array of strings.
-     * @return Array of strings: [ main content, armor content ]
-     * @throws IllegalStateException
-     */
-    public static String[] playerInventoryToBase64(PlayerInventory playerInventory) throws IllegalStateException {
-        //get the main content part, this doesn't return the armor
-        String content = toBase64(playerInventory);
-        String armor = itemStackArrayToBase64(playerInventory.getArmorContents());
-
-        return new String[]{content, armor};
-    }
-
-    /**
-     * A method to serialize an {@link ItemStack} array to Base64 String.
-     * <p>
-     * <p/>
-     * <p>
-     * Based off of {@link #toBase64(Inventory)}.
-     *
-     * @param items to turn into a Base64 String.
-     * @return Base64 string of the items.
-     * @throws IllegalStateException
-     */
     public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -62,85 +35,7 @@ public class BukkitSerialization {
         }
     }
 
-    /**
-     * A method to serialize an inventory to Base64 string.
-     * <p>
-     * <p/>
-     * <p>
-     * Special thanks to Comphenix in the Bukkit forums or also known
-     * as aadnk on GitHub.
-     *
-     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
-     *
-     * @param inventory to serialize
-     * @return Base64 string of the provided inventory
-     * @throws IllegalStateException
-     */
-    public static String toBase64(Inventory inventory) throws IllegalStateException {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-
-            // Write the size of the inventory
-            dataOutput.writeInt(inventory.getSize());
-
-            // Save every element in the list
-            for (int i = 0; i < inventory.getSize(); i++) {
-                dataOutput.writeObject(inventory.getItem(i));
-            }
-
-            // Serialize that array
-            dataOutput.close();
-            return Base64Coder.encodeLines(outputStream.toByteArray());
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to save item stacks.", e);
-        }
-    }
-
-    /**
-     * A method to get an {@link Inventory} from an encoded, Base64, string.
-     * <p>
-     * <p/>
-     * <p>
-     * Special thanks to Comphenix in the Bukkit forums or also known
-     * as aadnk on GitHub.
-     *
-     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
-     *
-     * @param data Base64 string of data containing an inventory.
-     * @return Inventory created from the Base64 string.
-     * @throws IOException
-     */
-    public static Inventory fromBase64(String data) throws IOException {
-        try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
-            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-            Inventory inventory = Bukkit.getServer().createInventory(null, dataInput.readInt());
-
-            // Read the serialized inventory
-            for (int i = 0; i < inventory.getSize(); i++) {
-                inventory.setItem(i, (ItemStack) dataInput.readObject());
-            }
-
-            dataInput.close();
-            return inventory;
-        } catch (ClassNotFoundException e) {
-            throw new IOException("Unable to decode class type.", e);
-        }
-    }
-
-    /**
-     * Gets an array of ItemStacks from Base64 string.
-     * <p>
-     * <p/>
-     * <p>
-     * Base off of {@link #fromBase64(String)}.
-     *
-     * @param data Base64 string to convert to ItemStack array.
-     * @return ItemStack array created from the Base64 string.
-     * @throws IOException
-     */
-    public static ItemStack[] itemStackArrayFromBase64(String data) throws IOException {
+    public static ItemStack[] base64ToItemStackArray(String data) throws IOException {
         try {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
             BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
@@ -156,5 +51,58 @@ public class BukkitSerialization {
         } catch (ClassNotFoundException e) {
             throw new IOException("Unable to decode class type.", e);
         }
+    }
+
+    // Turns the inventory contents and armor contents into base64 and then returns array of the 2 base64 strings
+    public static String[] turnInventoryIntoBase64s(Player player) {
+        List<ItemStack> inventoryContents = new ArrayList<>();
+        for (int i = 0; i < player.getInventory().getSize(); i++) {
+            ItemStack itemStack = player.getInventory().getItem(i);
+            if (itemStack == null || itemStack.getType().isAir()) {
+                itemStack = new ItemStack(org.bukkit.Material.AIR);
+            }
+            inventoryContents.add(itemStack);
+        }
+
+        List<ItemStack> armorContents = new ArrayList<>();
+        for (ItemStack itemStack : player.getInventory().getArmorContents()) {
+            if (itemStack == null || itemStack.getType().isAir()) {
+                itemStack = new ItemStack(org.bukkit.Material.AIR);
+            }
+            armorContents.add(itemStack);
+        }
+
+        String inventoryBase64 = BukkitSerialization.itemStackArrayToBase64(inventoryContents.toArray(new ItemStack[0]));
+        String armorBase64 = BukkitSerialization.itemStackArrayToBase64(armorContents.toArray(new ItemStack[0]));
+        return new String[]{inventoryBase64, armorBase64};
+    }
+
+    // Takes in the 2 base64 strings and turns them into the inventory contents and armor contents
+    public static void giveFromBase64s(Player player, String[] base64s) {
+        ItemStack[] inventoryItemStacks;
+        ItemStack[] armorItemStacks; // Not in use yet.
+        try {
+            inventoryItemStacks = BukkitSerialization.base64ToItemStackArray(base64s[0]);
+            armorItemStacks = BukkitSerialization.base64ToItemStackArray(base64s[1]); // Not in use yet.
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < player.getInventory().getSize(); i++) {
+            ItemStack itemStack = getItemStackIfExist(inventoryItemStacks, i);
+            if (itemStack == null || itemStack.getType().isAir()) continue;
+            if (player.getInventory().getItem(i) != null) {
+                player.getInventory().addItem(itemStack);
+            } else {
+                player.getInventory().setItem(i, itemStack);
+            }
+        }
+    }
+
+    private static ItemStack getItemStackIfExist(ItemStack[] itemStacks, int index) {
+        if (index >= 0 && index < itemStacks.length) {
+            return itemStacks[index];
+        }
+        return null;
     }
 }
