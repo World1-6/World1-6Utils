0ee5b440df17f2abf793c377cf0c32c92e14a225
Andrew121410 <andrewcherring@gmail.com>
2022-11-19 15:26:03 -0500

    Added component type methods in InventoryUtils

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/InventoryUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/InventoryUtils.java
@@ -1,29 +1,42 @@
 package com.andrew121410.mc.world16utils.utils;
 
 import com.andrew121410.mc.world16utils.chat.Translate;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InventoryUtils {
     public static ItemStack createItem(Material material, int amount, String displayName, String... loreString) {
-        List<String> lore = new ArrayList<>();
+        List<Component> loreList = new ArrayList<>();
+        for (String s : loreString) loreList.add(Translate.colorc(s));
+        Component[] loreArray = new Component[loreList.size()];
+        loreArray = loreList.toArray(loreArray);
+        return createItem(material, amount, Translate.colorc(displayName), loreArray);
+    }
+
+    public static ItemStack createItem(Material material, int amount, Component component, Component... lores) {
         ItemStack item = new ItemStack(material, amount);
         ItemMeta itemMeta = item.getItemMeta();
-        itemMeta.setDisplayName(Translate.color(displayName));
-        for (String s : loreString) lore.add(Translate.chat(s));
-        itemMeta.setLore(lore);
+        itemMeta.displayName(component);
+        List<Component> lore = new ArrayList<>(Arrays.asList(lores));
+        itemMeta.lore(lore);
         item.setItemMeta(itemMeta);
         return item;
     }
 
     public static ItemStack createItem(Material material, int amount, String displayName) {
+        return createItem(material, amount, Translate.colorc(displayName));
+    }
+
+    public static ItemStack createItem(Material material, int amount, Component component) {
         ItemStack item = new ItemStack(material, amount);
         ItemMeta itemMeta = item.getItemMeta();
-        itemMeta.setDisplayName(Translate.color(displayName));
+        itemMeta.displayName(component);
         item.setItemMeta(itemMeta);
         return item;
     }
