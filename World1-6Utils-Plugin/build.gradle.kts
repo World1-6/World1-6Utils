1ae854065b641bf476df5d72e3eac40d2a066bd0
Andrew121410 <andrewcherring@gmail.com>
2023-04-03 00:29:43 -0400

    Bump CCUtilsJava

--- World1-6Utils-Plugin/build.gradle.kts
+++ World1-6Utils-Plugin/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api(project(":World1-6Utils_WE_7210"))
 
     // Other
-    api("com.github.andrew121410:CCUtilsJava:0dc8d953")
+    api("com.github.andrew121410:CCUtilsJava:9faf6b65")
     compileOnly("org.geysermc.floodgate:api:2.0-SNAPSHOT")
     api("org.spongepowered:configurate-yaml:4.1.2")
 }
