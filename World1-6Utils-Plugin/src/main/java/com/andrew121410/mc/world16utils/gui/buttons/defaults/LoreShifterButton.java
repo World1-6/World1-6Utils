0b9d0dc6ba322135db11b2925b24dcd8d96ebd72
Andrew121410 <andrewcherring@gmail.com>
2023-05-30 21:33:36 -0400

    Have an option in LoreShifterButton to either have to confirm by right clicking, or not.

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/gui/buttons/defaults/LoreShifterButton.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/gui/buttons/defaults/LoreShifterButton.java
@@ -19,6 +19,8 @@ public class LoreShifterButton extends CloneableGUIButton {
 
     private BiConsumer<GUIClickEvent, Integer> onClickBiConsumer;
 
+    private boolean needToConfirm;
+
     private Component prefix = Translate.colorc("&b&l>&r ");
     private ItemStack itemStack;
     private List<Component> beforeLoreList; // This like extra stuff before the options.
@@ -28,11 +30,12 @@ public class LoreShifterButton extends CloneableGUIButton {
     private Component unModifiedLore;
     private Component modifiedLore;
 
-    public LoreShifterButton(int slot, ItemStack itemStack, List<Component> options, BiConsumer<GUIClickEvent, Integer> onClickBiConsumer) {
+    public LoreShifterButton(int slot, ItemStack itemStack, List<Component> options, boolean needToConfirm, BiConsumer<GUIClickEvent, Integer> onClickBiConsumer) {
         super(slot, itemStack);
         this.itemStack = itemStack;
         this.beforeLoreList = null;
         this.loreList = options;
+        this.needToConfirm = needToConfirm;
         this.onClickBiConsumer = onClickBiConsumer;
 
         Component firstLore = getLoreFromIndex(0);
@@ -45,8 +48,8 @@ public class LoreShifterButton extends CloneableGUIButton {
         refreshTheItem();
     }
 
-    public LoreShifterButton(int slot, ItemStack itemStack, List<Component> before, List<Component> options, BiConsumer<GUIClickEvent, Integer> onClickBiConsumer) {
-        this(slot, itemStack, options, onClickBiConsumer);
+    public LoreShifterButton(int slot, ItemStack itemStack, List<Component> before, List<Component> options, boolean needToConfirm, BiConsumer<GUIClickEvent, Integer> onClickBiConsumer) {
+        this(slot, itemStack, options, needToConfirm, onClickBiConsumer);
         this.beforeLoreList = before;
 
         refreshTheItem();
@@ -58,32 +61,53 @@ public class LoreShifterButton extends CloneableGUIButton {
         AbstractGUIWindow guiWindow = guiClickEvent.getGuiWindow();
         Player player = (Player) event.getWhoClicked();
 
-        if (event.getClick() == ClickType.RIGHT) {
-            this.loreList.set(this.lineNumber, this.unModifiedLore);
-            this.lineNumber++;
-
-            // Get the lore from the index, if it's null then reset it back to 0.
-            Component lore = getLoreFromIndex(this.lineNumber);
-            if (lore == null) {
-                this.lineNumber = 0;
-                lore = getLoreFromIndex(this.lineNumber);
+        if (needToConfirm) {
+            if (event.getClick() == ClickType.LEFT) {
+                move(guiClickEvent);
+            } else if (event.getClick() == ClickType.RIGHT) {
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1F, 1F);
+                this.onClickBiConsumer.accept(guiClickEvent, this.lineNumber);
             }
-
-            // Save and set the new lore.
-            this.unModifiedLore = lore;
-            this.modifiedLore = this.prefix.append(lore);
-            this.loreList.set(this.lineNumber, this.modifiedLore);
-
-            // Refresh the item with the new lore list.
-            refreshTheItem();
-
-            // Refresh
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1F, 1F);
-            guiWindow.refresh(player);
-        } else if (event.getClick() == ClickType.LEFT) {
-            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1F, 1F);
-            this.onClickBiConsumer.accept(guiClickEvent, this.lineNumber);
+        } else {
+            if (event.getClick() == ClickType.LEFT) {
+                move(guiClickEvent);
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1F, 1F);
+                this.onClickBiConsumer.accept(guiClickEvent, this.lineNumber);
+            }
         }
+    }
+
+    private void move(GUIClickEvent guiClickEvent) {
+        InventoryClickEvent event = guiClickEvent.getEvent();
+        AbstractGUIWindow guiWindow = guiClickEvent.getGuiWindow();
+        Player player = (Player) event.getWhoClicked();
+
+        this.loreList.set(this.lineNumber, this.unModifiedLore);
+        this.lineNumber++;
+
+        // Get the lore from the index, if it's null then reset it back to 0.
+        Component lore = getLoreFromIndex(this.lineNumber);
+        if (lore == null) {
+            this.lineNumber = 0;
+            lore = getLoreFromIndex(this.lineNumber);
+
+            // Shouldn't be null but just in case.
+            if (lore == null) {
+                return;
+            }
+        }
+
+        // Save and set the new lore.
+        this.unModifiedLore = lore;
+        this.modifiedLore = this.prefix.append(lore);
+        this.loreList.set(this.lineNumber, this.modifiedLore);
+
+        // Refresh the item with the new lore list.
+        refreshTheItem();
+
+        // Refresh
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1F, 1F);
+        guiWindow.refresh(player);
     }
 
     private void refreshTheItem() {
