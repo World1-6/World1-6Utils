ad017541e2034f37016d8833f9ef35680b99dd17
Andrew121410 <andrewcherring@gmail.com>
2023-06-10 16:12:25 -0400

    Forgot to update ClassWrappers, I might as well just delete the class, since I really only want to support the latest version any ways, I don't know yet.

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/ClassWrappers.java
@@ -3,18 +3,19 @@ package com.andrew121410.mc.world16utils.utils;
 import com.andrew121410.mc.world16utils.World16Utils;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit;
 import com.andrew121410.mc.world16utils.worldedit.WorldEdit_7210;
-import org.bukkit.Bukkit;
 
 public class ClassWrappers {
 
     private WorldEdit worldEdit = null;
 
     public ClassWrappers(World16Utils plugin) {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+//        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+//
+//        if (version.equals("v1_19_R2") || version.equals("v1_19_R3")) {//Extra
+//            this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_7210() : null;
+//        }
 
-        if (version.equals("v1_19_R2") || version.equals("v1_19_R3")) {//Extra
-            this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_7210() : null;
-        }
+        this.worldEdit = plugin.getServer().getPluginManager().getPlugin("WorldEdit") != null ? new WorldEdit_7210() : null;
     }
 
     public WorldEdit getWorldEdit() {
