a1781d20c5b5c37cd053efa60c8b5e5e2da8f11f
Andrew121410 <andrewcherring@gmail.com>
2024-07-07 15:29:51 -0400

    Change to World16HashBasedUpdater

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/Updater.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/Updater.java
@@ -1,8 +1,8 @@
 package com.andrew121410.mc.world16utils;
 
-import com.andrew121410.ccutils.utils.HashBasedUpdater;
+import com.andrew121410.mc.world16utils.updater.World16HashBasedUpdater;
 
-public class Updater extends HashBasedUpdater {
+public class Updater extends World16HashBasedUpdater {
 
     private static final String JAR_URL = "https://github.com/World1-6/World1-6Utils/releases/download/latest/World1-6Utils.jar";
     private static final String HASH_URL = "https://github.com/World1-6/World1-6Utils/releases/download/latest/hash.txt";
