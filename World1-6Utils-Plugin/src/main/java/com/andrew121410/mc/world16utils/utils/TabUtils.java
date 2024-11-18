1145eed45beea164063ea7a02a2a4fb5113807ab
Andrew121410 <andrewcherring@gmail.com>
2024-11-17 20:47:39 -0500

    Add TabUtils.getOfflinePlayerNames

--- World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/TabUtils.java
+++ World1-6Utils-Plugin/src/main/java/com/andrew121410/mc/world16utils/utils/TabUtils.java
@@ -1,7 +1,12 @@
 package com.andrew121410.mc.world16utils.utils;
 
+import org.bukkit.OfflinePlayer;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TabUtils {
     public static List<String> getContainsString(String args, List<String> list) {
@@ -12,5 +17,19 @@ public class TabUtils {
             }
         }
         return newList;
+    }
+
+    public static List<String> getOfflinePlayerNames(List<OfflinePlayer> offlinePlayers) {
+        return offlinePlayers.stream()
+                .filter(Objects::nonNull) // Filter out null OfflinePlayers
+                .filter(offlinePlayer -> offlinePlayer.getName() != null) // Filter out players with null names
+                .filter(offlinePlayer -> !offlinePlayer.getName().isEmpty()) // Filter out players with empty names
+                .filter(offlinePlayer -> !offlinePlayer.getName().equals("null")) // Filter out players with "null" as their name
+                .map(OfflinePlayer::getName) // Map to player names
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> getOfflinePlayerNames(OfflinePlayer[] offlinePlayers) {
+        return getOfflinePlayerNames(Arrays.asList(offlinePlayers));
     }
 }
