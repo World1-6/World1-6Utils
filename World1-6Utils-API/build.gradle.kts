4af59d94bee9d4cd50ab7d5a385fe40faa8db962
Andrew121410 <andrewcherring@gmail.com>
2026-04-04 19:23:59 -0400

    Fix jitpack again again??

--- World1-6Utils-API/build.gradle.kts
+++ World1-6Utils-API/build.gradle.kts
@@ -2,8 +2,17 @@ description = "World1-6Utils-API"
 
 plugins {
     id("world16utils.java-conventions")
+    `maven-publish`
 }
 
 dependencies {
     compileOnly(project(":World1-6Utils-COMMON"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
